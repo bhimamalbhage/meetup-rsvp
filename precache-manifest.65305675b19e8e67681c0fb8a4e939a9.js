85700f3f3416f9fcde1ce3f4bdd820e9d3bbac5d
Bhimashankar <bhimamalbhage@hotmail.com>
2020-10-08 15:08:24 +0530

    Updates

---
+++ precache-manifest.65305675b19e8e67681c0fb8a4e939a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a61bb905789c05cac112b893e81deb7",
+    "url": "/meetup-rsvp/index.html"
+  },
+  {
+    "revision": "b1124dad93f9bac35109",
+    "url": "/meetup-rsvp/static/css/main.1ded725e.chunk.css"
+  },
+  {
+    "revision": "b57732c91f345324a614",
+    "url": "/meetup-rsvp/static/js/2.382c9faf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/meetup-rsvp/static/js/2.382c9faf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1124dad93f9bac35109",
+    "url": "/meetup-rsvp/static/js/main.1a7bf82a.chunk.js"
+  },
+  {
+    "revision": "2054b562fcc7d03901a8",
+    "url": "/meetup-rsvp/static/js/runtime-main.1ace17a2.js"
+  }
+]);
